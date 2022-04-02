79f667389d3cc444b547697a49f7247d477acb93
Anaïs Gilbert <a.gilbertt3@hotmail.com>
2022-04-02 01:05:57 +0000

    Added server side code for creating subscriptions for pro users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :configure_premitted_parameters, if: :devise_controller?
+  
+  
+  protected
+    def configure_premitted_parameters
+      devise_parameter_sanitizer.permit(:signup) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
+    end
+  
 end
